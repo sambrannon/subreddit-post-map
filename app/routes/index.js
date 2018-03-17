b25187b4a2eea635c0ab73c1bad628d08741bdd8
Jimmy Brannon <james.brannon12@gmail.com>
2018-03-16 19:53:18 -0500

    add basic filtering for abbreviated states

--- app/routes/index.js
+++ app/routes/index.js
@@ -11,8 +11,9 @@ var redditUrl = "https://www.reddit.com/r/tattoos/new/.json";
 
 router.get("/", function(req, res){
 	reddit.getSubPosts(redditUrl)
+	.then(reddit.getPostsWithStateInTitle)
 	.then(function(posts){
-		res.send(posts);
+		return res.send(posts);
 	})
 });
 
--- helpers/reddit.js
+++ helpers/reddit.js
@@ -4,6 +4,9 @@ var express		= require("express"),
 	axios		= require("axios");
 
 
+var states = [ 'AL', 'AK', 'AS', 'AZ', 'AR', 'CA', 'CO', 'CT', 'DE', 'DC', 'FM', 'FL', 'GA', 'GU', 'HI', 'ID', 'IL', 'IN', 'IA', 'KS', 'KY', 'LA', 'ME', 'MH', 'MD', 'MA', 'MI', 'MN', 'MS', 'MO', 'MT', 'NE', 'NV', 'NH', 'NJ', 'NM', 'NY', 'NC', 'ND', 'MP', 'OH', 'OK', 'OR', 'PW', 'PA', 'PR', 'RI', 'SC', 'SD', 'TN', 'TX', 'UT', 'VT', 'VI', 'VA', 'WA', 'WV', 'WI', 'WY' ];	
+
+
 module.exports = {
 
 	getSubPosts: function(subUrl, limit, before, after){
@@ -19,9 +22,16 @@ module.exports = {
 		});
 	},
 
-	sayHello: function(){
-		return "Hello";
-	}
+	getPostsWithStateInTitle: function(posts){
+		return posts.filter(function(post){
+			return post.data.title		
+			.replace(/[.,?!_"';:-]/g, "")
+			.split(" ")
+			.some(function(str){
+				return states.includes(str);
+			});
+		});
+	}		
 
 }
 
