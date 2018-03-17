9ec774f545997579bbe96824caabcf7ce516ce92
Sam Brannon <sam@reverb.com>
2018-03-17 13:24:49 -0500

    Got a basic index view up with a demo map embed...

--- app/routes/index.js
+++ app/routes/index.js
@@ -2,20 +2,15 @@ var express = require("express"),
     router = express.Router(),
     reddit = require("../../helpers/reddit.js");
 
-
-var redditUrl = "https://www.reddit.com/r/tattoos/new/.json";
-
-
-
-
 router.get("/", function(req, res){
+  var redditUrl = "https://www.reddit.com/r/tattoos/new/.json";
+
   reddit.getSubPosts(redditUrl)
   .then(function(posts){
-    res.send(posts);
-  })
+    // res.send(posts);
+
+    res.render('index', {title: 'Reddit Map Thinger'});
+  });
 });
-
-
-
 
 module.exports = router;
