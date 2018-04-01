07bb4cb88e26a9c2cf2ab377f01e38d23d560fb8
Jimmy Brannon <james.brannon12@gmail.com>
2018-04-01 17:07:13 -0500

    only add post with image

--- helpers/mongoose.js
+++ helpers/mongoose.js
@@ -4,30 +4,32 @@ const mongoose = require("mongoose"),
 
 const help = {
 	addPostToDb: function(post){
-		const newPost = {
-			redditId: post.id,
-			subreddit: post.subreddit,
-			author: post.author,
-			image: {
-				source: post.preview.images[0].source,
-				small: post.preview.images[0].resolutions[0],
-				medium: post.preview.images[0].resolutions[3],
-				large: post.preview.images[0].resolutions[5],
-			},
-			title: post.title,
-			created_utc: post.created_utc,
-			city: post.city,
-			body: post.selftext
-		}
-		db.Post.create(newPost)
-		.then(function(post){
-			console.log(post);
-		})
-		.catch(function(err){
-			if(err.code !== 11000){
-				console.log(err);
+		if(post.preview && post.preview.images[0]){
+			const newPost = {
+				redditId: post.id,
+				subreddit: post.subreddit,
+				author: post.author,
+				image: {
+					source: post.preview.images[0].source,
+					small: post.preview.images[0].resolutions[0],
+					medium: post.preview.images[0].resolutions[3],
+					large: post.preview.images[0].resolutions[5],
+				},
+				title: post.title,
+				created_utc: post.created_utc,
+				city: post.city,
+				body: post.selftext
 			}
-		})
+			db.Post.create(newPost)
+			.then(function(post){
+				console.log("Post Added");
+			})
+			.catch(function(err){
+				if(err.code !== 11000){
+					console.log(err);
+				}
+			})
+		}
 	}
 }
 
